bd315ee0bde5e9b3f2c589070849ca71096eccdb
Rafael Ramalho Silveira <rafael@techrafael.com.br>
2025-05-27 17:39:44 -0400

    feat: setup inicial do projeto Spring Boot

---
+++ src/main/java/com/agrotis/teste_agrotis_backend/TesteAgrotisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agrotis.teste_agrotis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAgrotisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAgrotisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrotis/teste_agrotis_backend/TesteAgrotisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotis.teste_agrotis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAgrotisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
